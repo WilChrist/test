4ed4ef8f832a7b18ef02c1aee1270c8575616977
WilChrist <willynzesseu@gmail.com>
2018-03-15 13:31:19 +0000

    Ajout des model citizen et serviceprovider

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,15 @@ const passport = require('passport');
 // Bring in User Model
 let User = require('../models/user');
 
+// Bring in Citizen Model
+let Citizen = require('../models/citizen');
+
+// Bring in ServiceProvider Model
+let ServiceProvider = require('../models/serviceProvider');
+
+// Bring in Administrator Model
+//let Administrator = require('../models/administrator');
+
 // Register Form
 router.get('/register', function(req, res){
   res.render('register');
@@ -13,13 +22,20 @@ router.get('/register', function(req, res){
 
 // Register Proccess
 router.post('/register', function(req, res){
-  const name = req.body.name;
+  const first_name = req.body.first_name;
+  const last_name = req.body.last_name;
   const email = req.body.email;
   const username = req.body.username;
   const password = req.body.password;
   const password2 = req.body.password2;
+  const usertype = req.body.usertype;
+  const birth_date= req.body.birth_date;
+  const gender = req.body.gender;
+  const location = req.body.location;
+  //const registering_date = Date.now();
+let newUser;
 
-  req.checkBody('name', 'Name is required').notEmpty();
+  req.checkBody('first_name', 'First Name is required').notEmpty();
   req.checkBody('email', 'Email is required').notEmpty();
   req.checkBody('email', 'Email is not valid').isEmail();
   req.checkBody('username', 'Username is required').notEmpty();
@@ -33,12 +49,34 @@ router.post('/register', function(req, res){
       errors:errors
     });
   } else {
-    let newUser = new User({
-      name:name,
-      email:email,
-      username:username,
-      password:password
-    });
+    if(usertype == 'ADMINISTRATOR'){
+
+    }else if(usertype == 'SERVICEPROVIDER'){
+      newUser = new ServiceProvider({
+        first_name:first_name,
+        last_name:last_name,
+        gender:gender,
+        birth_date:birth_date,
+        location:location,
+        email:email,
+        username:username,
+        password:password,
+        company:"ensa"
+      });
+      console.log(newUser.password);
+    }else{
+        newUser = new Citizen({
+        first_name:first_name,
+        last_name:last_name,
+        gender:gender,
+        birth_date:birth_date,
+        location:location,
+        email:email,
+        username:username,
+        password:password
+      });
+    }
+    
 
     bcrypt.genSalt(10, function(err, salt){
       bcrypt.hash(newUser.password, salt, function(err, hash){
